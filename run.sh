0ab8095953ee12e2993f0ac5fbdfd53d53b71df6
SamuelMaciejewsky <samuel.maciejewsky@outlook.com>
2025-08-19 10:22:17 -0300

    a

--- run.sh
+++ run.sh
@@ -13,15 +13,21 @@ MINGW_LIB_PATH="./Dependencies/lib-mingw-w64"
 # Source code
 SRC_FILE="./src/main.cc"
 
+echo "Compilando o código com os seguintes parâmetros:"
+echo "INCLUDE_PATH: $INCLUDE_PATH"
+echo "MINGW_LIB_PATH: $MINGW_LIB_PATH"
+echo "SRC_FILE: $SRC_FILE"
+
 # Compile command
 g++ -I$INCLUDE_PATH -L$MINGW_LIB_PATH -lglfw3 -lopengl32 -lgdi32 -luser32 -lkernel32 $SRC_FILE -o build/$OUTPUT -D GLFW_DLL
 
 # Check Compile
-if [ $? -eq 0 ]; then
-    echo "Compilado com sucesso"
-else 
+if [ $? -ne 0 ]; then
     echo "Erro na compilação"
+    tail -n 20 build/$OUTPUT.log  # Mostra as últimas 20 linhas do arquivo de log de erro
     exit 1
+else
+    echo "Compilação bem-sucedida"
 fi
 
 # Copy DLL to build directory
