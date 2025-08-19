0bef51979c3f759f38943ac2da0acd81b0719f82
SamuelMaciejewsky <samuel.maciejewsky@outlook.com>
2025-08-19 12:05:17 -0300

    bostalikida

--- run.sh
+++ run.sh
@@ -7,22 +7,23 @@ OUTPUT="OpenGL_Test.exe"
 mkdir -p build
 
 # Path to Include and Lib
-INCLUDE_PATH="./Dependencies/include/"
-MINGW_LIB_PATH="./Dependencies/lib-mingw-w64/"
+INCLUDE_PATH="./Dependencies/include"
+MINGW_LIB_PATH="./Dependencies/lib-mingw-w64"
 
 # Source code
 SRC_FILE="./src/main.cc"
 
 # Compile command
-g++ -I$INCLUDE_PATH -L$MINGW_LIB_PATH -lglfw3 -lopengl32 -lgdi32 -luser32 -lkernel32 $SRC_FILE -o build/$OUTPUT -D GLFW_DLL
+g++ -I$INCLUDE_PATH -L$MINGW_LIB_PATH -lglfw3 -llibglfw3dll -lopengl32 -lgdi32 -luser32 -lkernel32 $SRC_FILE -o build/$OUTPUT -D GLFW_DLL
+
+EXIT_STATUS=$?
 
 # Check Compile
-if [ $? -eq 0 ]; then
-    echo "Compilado com sucesso"
-else 
-    echo "Erro na compilação"
-    exit 1
+if [ $EXIT_STATUS -ne 0 ]; then
+    echo "Compile error, Exit code: $EXIT_STATUS"
+    exit $EXIT_STATUS
+else
+    echo "Compilation Successfully!"
 fi
-
 # Copy DLL to build directory
 cp $LIB_PATH//glfw3.dll ./build/
