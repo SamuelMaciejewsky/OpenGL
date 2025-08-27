f181e131e85cdf41e23ef69d540be74f65000808
SamuelMaciejewsky <samuel.maciejewsky@outlook.com>
2025-08-27 02:52:07 -0300

    ABRIU KRL ABRIU DEU CERTO

---
+++ build/CMakeFiles/OpenGL_Test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGL_Test.dir/src/main.cc.obj"
+  "CMakeFiles/OpenGL_Test.dir/src/main.cc.obj.d"
+  "OpenGL_Test.exe"
+  "OpenGL_Test.exe.manifest"
+  "OpenGL_Test.pdb"
+  "libOpenGL_Test.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenGL_Test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
