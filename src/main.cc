375a56a803daeaab54adc1c9c3c4eda28365cbcd
SamuelMaciejewsky <samuel.maciejewsky@outlook.com>
2025-08-29 02:29:30 -0300

    aa

--- src/main.cc
+++ src/main.cc
@@ -1,42 +1,12 @@
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-#include <iostream>
+#include<glad/glad.h>
+#include<GLFW/glfw3.h>
 
-void error_callback(int error, const char* description){
-    std::cout << stderr << "Error: " << description << std::endl;
-}
+int main(void) {
 
-int main() {
-
-    GLFWwindow* window;
-    
-
-    glfwSetErrorCallback(error_callback);
-
-    if(!glfwInit()) {
-        
-        std::cout << "Eror to init glfw" << std::endl;
-        return -1;
-
-    }
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 0);
-
-    window = glfwCreateWindow(1280, 720, "Test", NULL, NULL);
-    if(!window) { 
-
-        std::cout << "Error to create window" << std::endl;
-        return -2;
-
-    }
-
-    glfwMakeContextCurrent(window);
-    gladLoadGL(glfwGetProcAddress);
-
-    glfwDestroyWindow(window);
-
-
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     return 0;
 
