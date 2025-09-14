1afc67afa5b92de28deca8f7e45f13eee9d70d02
SamuelMaciejewsky <samuel.maciejewsky@outlook.com>
2025-09-14 14:59:32 -0300

    Ajustando mais umas coisas ai

--- src/PreviousCodes/1.getting_started/1.7.shaders_uniform.cc
+++ src/PreviousCodes/1.getting_started/1.7.shaders_uniform.cc
@@ -34,10 +34,9 @@ unsigned int shaderPrograms[1];
 float vertices[] = {
 
     // First triangle
-    // Positions         // Colors
-    -0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,  // Bottom left
-     0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,  // Bottom right
-     0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f   // Top 
+    -0.5f, -0.5, 0.0f,  // Bottom left
+     0.5f, -0.5f, 0.0f,  // Bottom right
+     0.0f,  0.5f, 0.0f   // Top 
 
 };
 
@@ -53,27 +52,25 @@ float greenValue;
 int vertexColorLocation;
 
 const char* vertexShaderSource = "#version 460 core\n"
-
     "layout (location = 0) in vec3 aPos;\n"
-    "layout (location = 1) in vec3 aColor;\n"
 
-    "out vec3 ourColor;\n"
+    "out vec4 vertexColor;\n"
 
     "void main() {\n"
 
-    "   gl_Position = vec4(aPos, 1.0);\n"
-    "   ourColor = aColor;\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "   vertexColor = vec4(1.0f, 0.0, 0.20, 1.0f);\n"
 
     "}\0";
 
 const char* fragmentShaderSource = "#version 460 core\n"
 
     "out vec4 FragColor;\n"
-    "in vec3 ourColor;\n"
+    "uniform vec4 ourColor;\n"
 
     "void main() {\n"
 
-    "   FragColor = vec4(ourColor, 1.0);\n"
+    "   FragColor = ourColor;\n"
 
     "}\n\0";
 
@@ -225,12 +222,8 @@ int main(void) {
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     
     // 3. Set up vertex attributes
-    // Position attribute
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-    // Color attribute
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-    glEnableVertexAttribArray(1);
 
     // 4. Unbind to prevent accidental modification
     glBindBuffer(GL_ARRAY_BUFFER, 0);
@@ -238,7 +231,6 @@ int main(void) {
 
     // ------------------------------------------------------------------------------
 
-    glUseProgram(shaderPrograms[0]);
 
     // Engine loop - runs until the window is closed
     // ---------------------------------------------
@@ -252,6 +244,14 @@ int main(void) {
         glClear(GL_COLOR_BUFFER_BIT);
 
         // Draw here
+        glUseProgram(shaderPrograms[0]);
+
+        timeValue = glfwGetTime();
+        greenValue = (sin(timeValue) / 2.0f) + 0.5f;
+        vertexColorLocation = glGetUniformLocation(shaderPrograms[0], "ourColor");
+
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+
         glBindVertexArray(VAOs[0]);
         glDrawArrays(GL_TRIANGLES, 0, 3);
 
